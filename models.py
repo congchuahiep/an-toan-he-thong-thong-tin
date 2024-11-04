91ca44695ef162ee2a32964b79b28c74ea046afa
Trần Hoàng Hiệp <congchuahiep@gmail.com>
2024-11-05 00:00:22 +0700

    update database

--- models.py
+++ models.py
@@ -1,7 +1,11 @@
-from app import db
+from app import db, app
 
 # Định nghĩa mô hình người dùng
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
